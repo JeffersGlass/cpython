cc8c84c554ccb5b16c0e533858458afc0b8211cd
Jeff Glass <glass.jeffrey@gmail.com>
2024-01-23 20:40:15 -0600

    Improve process_scv

--- Tools/jit/process_csv.py
+++ Tools/jit/process_csv.py
@@ -9,14 +9,15 @@ with open(CPYTHON / "dual_opcode_data.csv", "r+") as infile, open(CPYTHON / "dua
     first_opcode_inst_count = defaultdict(int)
     for line in read_list:
         first_opcode_inst_count[line['first_opcode']] += int(line['combined'])
-        line['efficiency'] = int(line['combined'])/int(line['sum'])
+        line['efficiency'] = str(int(line['combined'])/int(line['sum']))
 
     ascending = sorted(read_list, key=lambda k: k['efficiency'])
 
-    fields = tuple(reader.fieldnames)
+    fields = list(reader.fieldnames)
+    fields.append('efficiency')
     outfile.write(','.join(fields))
     for line in ascending:
-        outfile.write(','.join(line[field] for field in fields))
+        outfile.write(','.join(line[field] for field in fields) + "\n")
 
     for line in ascending[500:600]:
         print(f"{round(int(line['combined'])/int(line['sum']), 2)} {line}")
