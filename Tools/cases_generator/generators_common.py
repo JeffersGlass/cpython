10e92bf32527ff8974bbca3eba079d35d196d3e3
Jeff Glass <glass.jeffrey@gmail.com>
2024-02-20 16:01:24 -0600

    Refactor determination of whether op uses operand (incorrect)

--- Tools/cases_generator/generators_common.py
+++ Tools/cases_generator/generators_common.py
@@ -213,6 +213,8 @@ def cflags(p: Properties) -> str:
         flags.append("HAS_PURE_FLAG")
     if p.passthrough:
         flags.append("HAS_PASSTHROUGH_FLAG")
+    if p.operand:
+        flags.append("HAS_OPERAND_FLAG")
     if flags:
         return " | ".join(flags)
     else:
